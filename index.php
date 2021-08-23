0111a07cc11c60dc959a329d2fb86c8d18695f4d
Maria13579 <patymsr3@gmail.com>
2021-08-23 11:16:16 -0600

    a

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    include('Autoload.php');
+    if(isset($_GET['class'])&& isset($_GET['method']))
+    {
+        $class=$_GET['class'];
+        $method=$_GET['method'];
+    }
+    else
+    {
+        $class="Home";
+        $method="Inicio";
+    }
+    if(class_exists($class)&& method_exists($class, $method))
+    {
+        $c= new $class();
+        $c-> $method();
+    }
+    else
+    {
+        echo "Clase o metodo no existente";
+    }
+   //?class=Inventario&method=ListaInventario
+    
+?>
+
